8062c2ce2e5783aa24a06e6984f3db0a6228d3c8
Rahul <chattychidiya@gmail.com>
2024-03-15 05:27:15 +0530

    solving cors problem

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ dotenv.config();
 
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+	origin: "*",
+	credentials: true,
+};
 
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
